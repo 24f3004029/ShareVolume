a5035ef7c4860484f88bf15c106e090026b9633d
24f3004029 <24f3004029@ds.study.iitm.ac.in>
2025-11-18 23:50:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "Building ShareVolume static site..."
+
+# Freeze Flask app into static files
 python3 -m flask_frozen freeze
+
 if [ ! -d "build" ]; then
-    echo "ERROR: Frozen-Flask did not generate build/ folder"
+    echo "ERROR: build/ folder not found"
     exit 1
 fi
+
 echo "Build generated successfully!"
